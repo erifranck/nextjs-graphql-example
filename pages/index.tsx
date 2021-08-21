ce578150b6ae277254a55eef1e75698bca1a059c
erifranck <erifranjose@hotmail.com>
2021-08-20 23:45:30 -0500

    add new paramethers to the query

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,23 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { LAUNCHES, LaunchResponse } from '../queries/space'
 import {useQuery} from '@apollo/client'
+import { Grid, GridItem } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   const {loading, error, data} = useQuery<LaunchResponse>(LAUNCHES)
   if (loading) return <div> loading... </div>
   if (error) return <div> { JSON.stringify(error) } </div>
   return (
-    <div>
+    <Grid templateColumns="repeat(6, 1fr)" gab={6}>
       {
         data && data.launchesPast.map(mission => (
-          <div key={mission.id}>
+          <GridItem key={mission.id}>
+            { mission.links.flickr_images.length && <img src={mission.links.flickr_images[0]} /> }
             <div>{mission.mission_name}</div>
-          </div>
+          </GridItem>
         ))
       }
-    </div>
+    </Grid>
   )
 }
 
--- queries/space.ts
+++ queries/space.ts
@@ -4,6 +4,10 @@ export interface Launch {
     id: string;
     mission_name: string;
     launch_date_local: string;
+    links: {
+        flickr_images: string[];
+        video_link: string;
+    }
 }
 
 export interface LaunchResponse {
@@ -12,7 +16,7 @@ export interface LaunchResponse {
 
 export const LAUNCHES = gql`
     {
-        launchesPast(limit: 10) {
+        launchesPast(limit: 30) {
             mission_name
             launch_date_local
             launch_site {
@@ -21,6 +25,7 @@ export const LAUNCHES = gql`
             links {
                 article_link
                 video_link
+                flickr_images
             }
             id
         }
